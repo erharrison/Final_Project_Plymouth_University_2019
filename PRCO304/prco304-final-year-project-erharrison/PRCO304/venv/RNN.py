005802be1d8849708afb330bae10aa7e6174f557
erharrison <emilypbi@gmail.com>
2019-03-12 15:08:04 +0000

    \more libraries imported into application and scaling implemented.

--- PRCO304/prco304-final-year-project-erharrison/PRCO304/venv/RNN.py
+++ PRCO304/prco304-final-year-project-erharrison/PRCO304/venv/RNN.py
@@ -1,5 +1,27 @@
 import numpy
+import pandas
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import keras
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 
-dataset = numpy.loadtxt(r"C:\Users\emily\Documents\GitHub\prco304-final-year-project-erharrison\PRCO304\prco304-final-year-project-erharrison\Data.csv", delimiter=",")
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
 
-inputX, outputY = dataset[1:-1,:], dataset[2:,:]
+dataframe = pandas.read_csv(
+    r"C:\Users\emily\Documents\GitHub\prco304-final-year-project-erharrison\PRCO304\
+    prco304-final-year-project-erharrison\Data.csv",
+    delimiter=",",
+    engine='python')
+
+dataset = dataframe.values
+# floating point values are more suitable for neural networks
+dataset = dataset.astype('float32')
+
+inputX, outputY = dataset[1:-1, :], dataset[2:, :]
