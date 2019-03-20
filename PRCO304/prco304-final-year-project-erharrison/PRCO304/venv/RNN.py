73a8881734e54a3b5ee207f78e9c8af339145d03
erharrison <emilypbi@gmail.com>
2019-03-20 11:29:08 +0000

    Heatmap plotted but it looks very crowded. Needs fixing. Also trying to implement TensorBoard.

--- PRCO304/prco304-final-year-project-erharrison/PRCO304/venv/RNN.py
+++ PRCO304/prco304-final-year-project-erharrison/PRCO304/venv/RNN.py
@@ -1,17 +1,16 @@
-import numpy
-import pandas
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import keras
-import math
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-from tensorflow.python.ops import rnn, rnn_cell
+# TODO complete exception report
 
-look_back = 1
+import pandas
+import keras
+import matplotlib
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Flatten
+from sklearn.preprocessing import MinMaxScaler
+import numpy
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import TensorBoard
+import time
+
 
 # fix random seed for reproducibility
 seed = 7
@@ -46,9 +45,9 @@ testX = testX.reshape(testX.shape[0], 1, testX.shape[1])
 testY = testY.reshape(testY.shape[0], 1, testY.shape[1])
 
 # create RNN model
+# Sequential model is a linear stack of layers
 model = Sequential()
-model.add(
-keras.layers.SimpleRNN(77, activation='relu', use_bias=True, kernel_initializer='he_normal', return_sequences=True))
+model.add(keras.layers.SimpleRNN(77, activation='relu', use_bias=True, kernel_initializer='he_normal', return_sequences=True))
 model.add(Dense(77, activation='relu'))
 
 model.compile(loss='mean_squared_error',
@@ -58,7 +57,13 @@ model.compile(loss='mean_squared_error',
                        'mean_squared_logarithmic_error',  # used to measure difference between actual and predicted
                        'mean_absolute_error'])  # measure how close predictions are to output])
 
-model.fit(trainX, trainY, epochs=1000, batch_size=len(trainX), verbose=2)
+name = "recurrent-neural-network-{}".format(int(time.time()))
+
+tensorboard = TensorBoard(log_dir='/TensorBoardResults/logs/{}'.format(name), histogram_freq=0,
+                          write_graph=True)
+
+model.fit(trainX, trainY, epochs=1000, batch_size=len(trainX), verbose=2, callbacks=[tensorboard])
+model.fit(testX, testY, epochs=1000, batch_size=len(testX), verbose=2, callbacks=[tensorboard])
 
 # make predictions
 trainPredict = model.predict(trainX)
@@ -66,3 +71,124 @@ testPredict = model.predict(testX)
 
 print(trainPredict)
 print(testPredict)
+
+
+def heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw={}, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Arguments:
+        data       : A 2D numpy array of shape (N,M)
+        row_labels : A list or array of length N with the labels
+                     for the rows
+        col_labels : A list or array of length M with the labels
+                     for the columns
+    Optional arguments:
+        ax         : A matplotlib.axes.Axes instance to which the heatmap
+                     is plotted. If not provided, use current axes or
+                     create a new one.
+        cbar_kw    : A dictionary with arguments to
+                     :meth:`matplotlib.Figure.colorbar`.
+        cbarlabel  : The label for the colorbar
+    All other arguments are directly passed on to the imshow call.
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    ax.set_xticks(numpy.arange(data.shape[100]))
+    ax.set_yticks(numpy.arange(data.shape[0]))
+    # ... and label them with the respective list entries.
+    ax.set_xticklabels(col_labels)
+    ax.set_yticklabels(row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(top=True, bottom=False,
+                   labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+             rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    for edge, spine in ax.spines.items():
+        spine.set_visible(False)
+
+    ax.set_xticks(numpy.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(numpy.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=["black", "white"],
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Arguments:
+        im         : The AxesImage to be labeled.
+    Optional arguments:
+        data       : Data used to annotate. If None, the image's data is used.
+        valfmt     : The format of the annotations inside the heatmap.
+                     This should either use the string format method, e.g.
+                     "$ {x:.2f}", or be a :class:`matplotlib.ticker.Formatter`.
+        textcolors : A list or array of two color specifications. The first is
+                     used for values below a threshold, the second for those
+                     above.
+        threshold  : Value in data units according to which the colors from
+                     textcolors are applied. If None (the default) uses the
+                     middle of the colormap as separation.
+
+    Further arguments are passed on to the created text labels.
+    """
+
+    if not isinstance(data, (list, numpy.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[im.norm(data[i, j]) > threshold])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
+
+fig, ax = plt.subplots()
+
+im, cbar = heatmap(dataset, 'time', 'no. of samples', ax=ax,
+                   cmap="YlGn", cbarlabel="infected samples [t/year]")
+texts = annotate_heatmap(im, valfmt="{x:.1f} t")
+
+fig.tight_layout()
+plt.show()
