e7dae39d26c09b711b635c9372ea7126e5c8b7c2
erharrison <emilypbi@gmail.com>
2019-04-28 16:41:18 +0100

    Reading Excel file instead of csv.

--- PRCO304/prco304-final-year-project-erharrison/PRCO304/venv/NN.py
+++ PRCO304/prco304-final-year-project-erharrison/PRCO304/venv/NN.py
@@ -22,14 +22,14 @@ numpy.random.seed(seed)
 os.environ["PATH"] += os.pathsep + 'C:/Users/emily/Downloads/graphviz-2.38/release/bin'
 
 map_image_path = r'C:\Users\emily\Documents\GitHub\prco304-final-year-project-erharrison\PRCO304\prco304-final-year-project-erharrison\Map.jpg'
-data_file_path = r'C:\Users\emily\Documents\GitHub\prco304-final-year-project-erharrison\PRCO304\prco304-final-year-project-erharrison\Data.csv'
+data_file_path = r'C:\Users\emily\Documents\GitHub\prco304-final-year-project-erharrison\PRCO304\prco304-final-year-project-erharrison\Data.xlsx'
 
 # creating image for map of North America for data to be visualised on
 map_img = mpimg.imread(map_image_path)
 
-dataframe = pandas.read_csv(data_file_path,
-    delimiter=",",
-    engine='python')
+data_file = pandas.ExcelFile(data_file_path)
+
+dataframe = data_file.parse('Sheet1')
 
 dataset = dataframe.values
 # floating point values are more suitable for neural networks
@@ -129,8 +129,8 @@ plt.show();
 trainPredict = numpy.ndarray.transpose(trainPredict)
 
 # iterating through arrays of locations
-for i in numpy.nditer(trainPredict):
-    print(trainPredict)
+# for i in numpy.nditer(trainPredict):
+#     print(trainPredict)
 #     # Creating circles on map to visualise predictions
 #     cv2.circle(map_img, (200, 200), trainPredict[0, 0, 0], (0, 20, 200), 2)
 #     cv2.imshow('Map', map_img)
