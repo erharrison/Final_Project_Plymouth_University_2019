61254ad2d7d9e1e51de9a33aaa05f9443365cb35
erharrison <emilypbi@gmail.com>
2019-04-28 20:55:15 +0100

    Small changes to check that csv and excel read had exactly the same output.

--- PRCO304/prco304-final-year-project-erharrison/PRCO304/venv/NN.py
+++ PRCO304/prco304-final-year-project-erharrison/PRCO304/venv/NN.py
@@ -22,25 +22,34 @@ numpy.random.seed(seed)
 os.environ["PATH"] += os.pathsep + 'C:/Users/emily/Downloads/graphviz-2.38/release/bin'
 
 map_image_path = r'C:\Users\emily\Documents\GitHub\prco304-final-year-project-erharrison\PRCO304\prco304-final-year-project-erharrison\Map.jpg'
-data_file_path = r'C:\Users\emily\Documents\GitHub\prco304-final-year-project-erharrison\PRCO304\prco304-final-year-project-erharrison\Data.xlsx'
+# data_file_path_csv = r'C:\Users\emily\Documents\GitHub\prco304-final-year-project-erharrison\PRCO304\prco304-final-year-project-erharrison\Data.csv'
 coordinates_file_path = r'C:\Users\emily\Documents\GitHub\prco304-final-year-project-erharrison\PRCO304\prco304-final-year-project-erharrison\Coordinates.xlsx'
+data_file_path_excel = r'C:\Users\emily\Documents\GitHub\prco304-final-year-project-erharrison\PRCO304\prco304-final-year-project-erharrison\Data.xlsx'
+
+# Reading Excel file and spreadsheet of original data
+data_file_excel = pandas.read_excel(data_file_path_excel, sheet_name='Data')
+# Creating dataframe from data and selecting columns
+dataframe_excel = pandas.DataFrame(data_file_excel)
+dataset_excel = dataframe_excel.values
+# floating point values are more suitable for neural networks
+dataset_excel = dataset_excel.astype('float32')
 
 # creating image for map of North America for data to be visualised on
 map_img = mpimg.imread(map_image_path)
 
-# Reading Excel file and spreadsheet of original data
-data_file = pandas.read_excel(data_file_path, sheet_name='Data')
-# Creating dataframe from data and selecting columns
-dataframe_data = pandas.DataFrame(data_file)
 
-# data_file = pandas.ExcelFile(data_file_path)
-# dataframe = data_file.parse('Sheet1')
-dataset = dataframe_data.values
-# floating point values are more suitable for neural networks
-dataset = dataset.astype('float32')
+# data_file = pandas.read_excel(data_file_path, sheet_name='Data')
+# dataframe_data = pandas.DataFrame(data_file)
+
+
+# data_file_csv = pandas.read_csv(data_file_path_csv,
+#     delimiter=",",
+#     engine='python')
+# dataframe_csv = pandas.DataFrame(data_file_excel)
+
 
 scaler = MinMaxScaler(feature_range=(0, 1))  # MinMaxScalar is from scikit learn library
-dataset = scaler.fit_transform(dataset)
+dataset = scaler.fit_transform(dataset_excel)
 
 # split into train and test sets
 train_size = int(len(dataset) * 0.8)  # 80% - train_size = 84
@@ -137,8 +146,8 @@ coordinates = coordinates.astype('float32')
 trainPredict = numpy.ndarray.transpose(trainPredict)
 
 # iterating through arrays of locations
-# for i in numpy.nditer(trainPredict):
-#     print(trainPredict)
+for i in numpy.nditer(trainPredict):
+    print(trainPredict[0, 1, i], coordinates)
 #     # Creating circles on map to visualise predictions
 #     cv2.circle(map_img, (200, 200), trainPredict[0, 0, 0], (0, 20, 200), 2)
 #     cv2.imshow('Map', map_img)
